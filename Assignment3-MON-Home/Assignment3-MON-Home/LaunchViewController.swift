25a4ff4ae66c827bb5cd1b17ca36878eb30b5935
hhchenweicheng@gmail.com <a13135110864>
2017-11-05 17:07:08 +1100

    ...

--- Assignment3-MON-Home/Assignment3-MON-Home/LaunchViewController.swift
+++ Assignment3-MON-Home/Assignment3-MON-Home/LaunchViewController.swift
@@ -23,7 +23,7 @@ class LaunchViewController: UIViewController, PaperOnboardingDataSource {
     }
     
     func onboardingItemsCount() -> Int {
-        return 3
+        return 4
     }
     
     func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
@@ -31,15 +31,18 @@ class LaunchViewController: UIViewController, PaperOnboardingDataSource {
         let backgroundColorOne = UIColor(red: 217/255, green: 72/255, blue: 89/255, alpha: 1)
         let backgroundColorTwo = UIColor(red: 106/255, green: 166/255, blue: 211/255, alpha: 1)
         let backgroundColorThree = UIColor(red: 168/255, green: 200/255, blue: 78/255, alpha: 1)
+        let backgroundColorFour = UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)
         
         let titleFont = UIFont(name: "AvenirNext-Bold", size: 24)!
         let descirptionFont = UIFont(name: "AvenirNext-Regular", size: 18)!
         
-        return [(#imageLiteral(resourceName: "rocket"), "A Great Rocket Start", "Caramels cheesecake bonbon bonbon topping. Candy halvah cotton candy chocolate bar cake. Fruitcake liquorice candy canes marshmallow topping powder.", #imageLiteral(resourceName: "circle"), backgroundColorOne, UIColor.white, UIColor.white, titleFont, descirptionFont),
+        return [(#imageLiteral(resourceName: "launchHome"), "Smart Home", "Add then config your devices.", #imageLiteral(resourceName: "circle"), backgroundColorOne, UIColor.white, UIColor.white, titleFont, descirptionFont),
                 
-                (#imageLiteral(resourceName: "brush"), "Design your Experience", "Caramels cheesecake bonbon bonbon topping. Candy halvah cotton candy chocolate bar cake. Fruitcake liquorice candy canes marshmallow topping powder.", #imageLiteral(resourceName: "circle"), backgroundColorTwo, UIColor.white, UIColor.white, titleFont, descirptionFont),
+                (#imageLiteral(resourceName: "launchWeather"), "Weather", "Check the current weather and the weather forecase.", #imageLiteral(resourceName: "circle"), backgroundColorTwo, UIColor.white, UIColor.white, titleFont, descirptionFont),
                 
-                (#imageLiteral(resourceName: "notification"), "Stay Up To Date", "Get notified of important updates.", #imageLiteral(resourceName: "circle"), backgroundColorThree, UIColor.white, UIColor.white, titleFont, descirptionFont)][index]
+                (#imageLiteral(resourceName: "launchVoice"), "Voice Control", "Turn on/off your devices by voice.", #imageLiteral(resourceName: "circle"), backgroundColorThree, UIColor.white, UIColor.white, titleFont, descirptionFont),
+                
+                (#imageLiteral(resourceName: "launchSetting"), "Settings", "Add flic button to control your devices and config your sensors.", #imageLiteral(resourceName: "circle"), backgroundColorFour, UIColor.white, UIColor.white, titleFont, descirptionFont)][index]
     }
     
     func onboardingConfigurationItem(_ item: OnboardingContentViewItem, index: Int) {
